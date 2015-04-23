26892b7d1b15d3cdbcced92b8411b93bc284853e
rain <563268276@qq.com>
2015-04-23 15:47:43 +0800

    Update gather.php
    修改采集程序，将配置移动到外面的ini文件中

--- gather.php
+++ gather.php
@@ -1,70 +1,58 @@
 <?php
 /**
- *  可以灵活配置使用的采集器
- *  作者：Rain
- *  创建时间：2015-02-03 15:17:30
- *  版本信息：V1.0
+	; 一个针对程序员编写的简单的网页采集工具
+	; 目前只支持不需要进行登录及权限认证即可访问的页面资源的采集功能，包括文章、下载资源等
+	; 目前暂时不支持IP代理功能，有需要的朋友可以自己进行扩展
+	; 目前暂时不支持只提供临时下载地址的资源的采集，即没有做本地化的处理功能
+	;
+	; 功能描述
+	; 1.支持多次跳转的资源下载功能
+	; 2.支持采集内容的字符替换功能
+	; 3.支持断点继续采集功能，根据设置PAGE_START参数
+	;
+	; 作者：Rain
+	; 联系QQ：563268276
+	; 网址：www.94cto.com / www.itziy.com / www.verypan.com
+	; 时间：2015-04-23
+	; 版本：V2
+	; 版权：可以进行任意修改，再发布及自己学习使用，不可用于非法用途，否则后果自负
+	;
+	; 使用方式
+		# 默认的配置文件名称是当前执行目录下的conf.ini
+		# php gather.php conf.ini
+	
+	; 温馨提示：默认情况下，我们认定Linux系统默认命令行编码为utf-8，window系统命令行的编码为GBK编码
 */
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//数据库的相关配置信息,请根据您的数据库信息进行配置
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PWD', 'testpwd');
-define('DB_NAME', 'test_db');
-define('DB_CHARSET', 'utf8');
-define('TABLE_NAME', 'test_table_name');
-//end
+$conf_file = dirname(__FILE__).'/conf.ini';
 
-//网站信息相关的配置,请根据具体需要采集的网站内容信息进行配置
-define('WEB_CHARSET', 'gbk');
-//变动的参数，使用%d进行替换,只支持数值形式的变动
-define('WEB_LIST_URL', 'http://down.chinaz.com/class/3_%d.htm');
-//分页的条数
-define('PAGE_COUNT', 1034);
-//从哪个页面开始抓取
-define('PAGE_START', 225);
-//内容页的URL,使用正则模式,必须包含/,例如：/\/xuefu2008\/article\/details\/(\d)+/i
-define('WEB_CONTENT_URL_REG', '/"image"><a\shref="(\/soft\/(\d)+\.htm)/i');
-//网站域名HOST信息,不包含末尾的/，例如：http://blog.csdn.net
-define('WEB_HOST', 'http://down.chinaz.com');
-//列表页内容的精准定位，用来大致抓取一个列表页的内容显示模块位置，使用正则进行定位
-define('WEB_LIST_POSTION', '/<div\sclass="panel\-body">.*?software\-item">(.*?)<div\sclass="pagination">/i');
-//end
+if (isset($argv[1]) && ($argv[1] == 'h' || $argv[1] == 'help' || $argv[1] == '-h' || $argv[1] == '--help'))
+	die('usage: php gather.php conf.ini');
 
-//微调参数，通常不修改也不会影响您的正常使用
-define('SLEEP_TIME', 2);
-define('IS_DEBUG', false);
-define('INSERT_DB', true);
-//内容的输出速度，单位：秒
-define('OUTPUT_SPEED', 0);
-//end
+if (isset($argv[1]))
+	$conf_file = $argv[1];
+if (!file_exists($conf_file))
+	die('configuration file: '.$conf_file.' not find');
 
-//需要过滤删除的文字,根据采集的网站类型进行设置,不区分大小写
-$text_filter = array(
-	'_网站源码' => '',
-	'_站长下载' => '',
-	'下载' => '',
-);
+$confArr = parse_ini_file($conf_file, true);
+if (!is_array($confArr) || empty($confArr))
+	die('configuration file: '.$conf_file.' invalid');
 
-//表结构映射的配置
-$table_mapping = array(
-//表字段名称 => 获取该字段的正则表达式,非空字段都必须在此设置映射关系，常量值请直接填写具体对应的数值，无需使用正则
-	'size' => '/软件大小.*?>(.*?)<\/li>/i',
-	'logo' => 'http://www.94cto.com/index/uploads/images/20141128/4e3525a57fb066c0bf892fc3fd4eb443.gif',
-	'subject' => '/<title>(.*?)<\/title>/i',
-	'content' => '/software\-intro">(.*?)<\/div>.*?<div\sclass="text_tag">/i',
-	'lid' => '1',
-	'uid' => '1',
-	'isadmin' => '1',
-	'tags' => '源码,计算机,web',
-	'platform' => 'window/Linux',
-	'ishot' => '1',
-	'agreement' => '免费',
-	'downurl' => '/\'(\/download\.asp\?id=\d+\&dp=1\&fid=10\&f=yes)\'/i',
-	'istop' => '1',
-);
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (!isset($confArr['CONST']) || !isset($confArr['VARIABLE']))
+	die('configuration file: '.$conf_file.' invalid');
+
+//init const variable
+foreach ($confArr['CONST'] as $ck => $cv)
+{
+	if (!defined($ck))
+		define($ck, $cv);
+}
+
+//init global variable
+foreach ($confArr['VARIABLE'] as $vk => $vv)
+{
+	$$vk = $vv;
+}
 
 $ga = new Gather();
 $ga->run();
@@ -78,11 +66,10 @@ class Gather
 
 	public function run()
 	{
-		global $table_mapping, $text_filter;
+		global $table_mapping, $text_filter, $preDownArr, $downArr, $need_host_check_field_name;
 
 		for ($page = PAGE_START; $page <= PAGE_COUNT; $page++)
 		{
-
 			$this->write('开始采集列表第'.$page.'页的内容...');
 			$list_content = $this->get(sprintf(WEB_LIST_URL, $page));
 			if (empty($list_content))
@@ -125,6 +112,8 @@ class Gather
 					$list_url = $val;
 					$this->write('当前抓取第'.($kval + 1).'条内容记录');
 					$web_content = $this->get($val);
+					$content_url = $val;
+
 					if (empty($web_content))
 					{
 						$this->write('抓取的内容页为空,所以过滤掉');
@@ -163,12 +152,19 @@ class Gather
 						{
 							if (!preg_match($reg, $web_content, $tmp_match))
 							{
-								$this->write('对不起,匹配字段：'.$field.'失败，过滤此记录');
-								continue 2;
+								if (defined('ITEM_IMAGE_FIELD_NAME') && ITEM_IMAGE_FIELD_NAME == $field)
+									$$field = ITEM_DEFAULT_IMG;
+								else
+								{
+									$this->write('对不起,匹配字段：'.$field.'失败，过滤此记录');
+									continue 2;
+								}
 							}
 
-							$$field = $tmp_match[1];
+							if (isset($tmp_match[1]))
+								$$field = $tmp_match[1];
 							$$field = $this->closetags($$field);
+							$$field = trim($$field);
 							
 							//删除javascript脚本
 							$$field = preg_replace('/<script(.*?)>(.*?)<\/script>/i', '', $$field);
@@ -214,7 +210,10 @@ class Gather
 								$$field = str_ireplace($tk, $tv, $$field);
 						}
 
-						if ('downurl' == $field && stripos($$field, 'http:') === false)
+						if (defined('ITEM_IMAGE_FIELD_NAME') && ITEM_IMAGE_FIELD_NAME == $field && (empty($$field) || trim($$field) == '#'))
+							$$field = ITEM_DEFAULT_IMG;
+
+						if (defined('ITEM_IMAGE_FIELD_NAME') && ITEM_IMAGE_FIELD_NAME == $field && stripos($$field, 'http:') === false)
 						{
 							if (substr($$field, 0, 1) == '/')
 								$$field = WEB_HOST.trim($$field);
@@ -222,21 +221,111 @@ class Gather
 								$$field = WEB_HOST.'/'.trim($$field);
 						}
 
-						if ('downurl' == $field && (stripos($$field, '&') !== false))
+						if (is_array($need_host_check_field_name) && !empty($need_host_check_field_name) && in_array($field, $need_host_check_field_name) && stripos($$field, 'http:') === false)
 						{
-							$result_tmp = $this->get($$field, array('Referer: '.$list_url), true);
-							if (stripos($result_tmp, 'Location:')  !== false)
+							if (substr($$field, 0, 1) == '/')
+								$$field = WEB_HOST.trim($$field);
+							else
+								$$field = WEB_HOST.'/'.trim($$field);
+						}
+
+						if (defined('ITEM_DOWNLOAD_FIELD_NAME') && ITEM_DOWNLOAD_FIELD_NAME == $field)
+						{
+							if (!empty($preDownArr) && is_array($preDownArr))
 							{
-								if (IS_DEBUG)
-									$this->write("检查到有跳转的信息\n*****************************************************************************\n".$result_tmp."\n******************************************************************************");
-								preg_match('/Location:(.*?)\r/i', $result_tmp, $url_tmp_match);
-								if (isset($url_tmp_match[1]))
-									$$field = trim($url_tmp_match[1]);
+								$is_find_pre_down_url = false;
+								foreach ($preDownArr as $pdk => $pdv)
+								{
+									if (stripos($$field, 'http:') === false)
+										$pdv .= $$field;
+
+									$pdv_content = $this->get($pdv, array('Referer: '.$content_url), false);
+									$pdv_content = str_replace("\r", '', $pdv_content);
+									$pdv_content = str_replace("\n", '', $pdv_content);
+
+									if (!preg_match($pdk, $pdv_content, $pdv_match))
+										continue;
+
+									$$field = trim(urldecode($pdv_match[1]));
+									$is_find_pre_down_url = true;
+									break;
+								}
+
+								if (!$is_find_pre_down_url)
+								{
+									$this->write("匹配预下载的地址失败，所以过滤此记录");
+									continue 2;
+								}
 							}
 						}
 
-						//对于下载的url地址，downurl做最后一次判断
-						if ('downurl' == $field)
+						if (defined('ITEM_DOWNLOAD_FIELD_NAME') && ITEM_DOWNLOAD_FIELD_NAME == $field && stripos($$field, 'http:') === false)
+						{
+							if (is_array($downArr) && !empty($downArr))
+							{
+								foreach ($downArr as $d_url)
+								{
+									if (substr($$field, 0, 1) != '/')
+										$$field = '/'.$$field;
+
+									$d_url .= $$field;
+
+									if (defined('ITEM_DOWNLOAD_FIELD_NAME') && ITEM_DOWNLOAD_FIELD_NAME == $field && (stripos($d_url, '&') !== false))
+									{
+										$find_real_down = false;
+										$result_tmp = $this->get($d_url, array('Referer: '.$list_url), true);
+										if (stripos($result_tmp, 'Location:')  !== false)
+										{
+											if (IS_DEBUG)
+												$this->write("检查到有跳转的信息\n*****************************************************************************\n".$result_tmp."\n******************************************************************************");
+											preg_match('/Location:(.*?)\r/i', $result_tmp, $url_tmp_match);
+											if (isset($url_tmp_match[1]))
+											{
+												$find_real_down = true;
+												$$field = trim($url_tmp_match[1]);
+												break;
+											}
+											else
+												continue;
+										}
+
+										if (!$find_real_down)
+										{
+											$this->write('下载地址:'.$d_url.'不存在location，所以过滤此记录');
+											continue 3;
+										}
+									}
+									else
+									{
+										$find_real_down = true;
+										break;
+									}
+								}
+							}
+							else
+							{
+								if (substr($$field, 0, 1) == '/')
+									$$field = WEB_HOST.trim($$field);
+								else
+									$$field = WEB_HOST.'/'.trim($$field);
+
+								if (defined('ITEM_DOWNLOAD_FIELD_NAME') && ITEM_DOWNLOAD_FIELD_NAME == $field && (stripos($$field, '&') !== false))
+								{
+									$result_tmp = $this->get($$field, array('Referer: '.$list_url), true);
+									if (stripos($result_tmp, 'Location:')  !== false)
+									{
+										if (IS_DEBUG)
+											$this->write("检查到有跳转的信息\n*****************************************************************************\n".$result_tmp."\n******************************************************************************");
+										preg_match('/Location:(.*?)\r/i', $result_tmp, $url_tmp_match);
+										if (isset($url_tmp_match[1]))
+											$$field = trim($url_tmp_match[1]);
+									}
+								}
+							}
+						}
+
+						//对于下载的url地址，做最后一次判断
+						if (defined('ITEM_DOWNLOAD_FIELD_NAME') && ITEM_DOWNLOAD_FIELD_NAME == $field)
 						{
 							if (IS_DEBUG)
 								$this->write($$field);
@@ -245,12 +334,24 @@ class Gather
 								$this->write('下载地址:'.$$field.'无效，所以过滤此记录');
 								continue 2;
 							}
-							if ($url_ret['Content-Length'] < 100)
+
+							if (stripos($url_ret[0], '200 OK') === false)
 							{
-								$this->write('下载地址:'.$$field.'返回内容大小为'.$url_ret['Content-Length'].'，所以过滤此记录');
+								$this->write('下载地址:'.$$field.'返回404错误，所以过滤此记录');
 								continue 2;
 							}
+
+							if (stripos($$field, 'pan.baidu.com') === false)
+							{
+								if ($url_ret['Content-Length'] < 100)
+								{
+									$this->write('下载地址:'.$$field.'返回内容大小为'.$url_ret['Content-Length'].'，所以过滤此记录');
+									continue 2;
+								}
+							}
 						}
+
+						$$field = trim($$field);
 
 						if (IS_DEBUG)
 							$this->write('* '.'字段：'.$field.'  值：'."\n****************************************************************\n".$$field."\n****************************************************************");
@@ -286,25 +387,30 @@ class Gather
 		$closedtags = $result[1]; 
 		// 计算关闭开启标签数量，如果相同就返回html数据 
 		$len_opened = count($openedtags); 
-		if (count($closedtags) == $len_opened) { 
-		return $html; 
-		} 
+		if (count($closedtags) == $len_opened)
+			return $html; 
+
 		// 把排序数组，将最后一个开启的标签放在最前面 
 		$openedtags = array_reverse($openedtags); 
 		// 遍历开启标签数组 
-		for ($i = 0; $i < $len_opened; $i++) { 
-		// 如果需要补全的标签 
-		if (!in_array($openedtags[$i], $arr_single_tags)) { 
-		// 如果这个标签不在关闭的标签中 
-		if (!in_array($openedtags[$i], $closedtags)) { 
-		// 直接补全闭合标签 
-		$html .= '</' . $openedtags[$i] . '>'; 
-		} else { 
-		unset($closedtags[array_search($openedtags[$i], $closedtags)]); 
-		} 
-		} 
-		} 
-	return $html; 
+		for ($i = 0; $i < $len_opened; $i++)
+		{
+			// 如果需要补全的标签 
+			if (!in_array($openedtags[$i], $arr_single_tags))
+			{
+				// 如果这个标签不在关闭的标签中 
+				if (!in_array($openedtags[$i], $closedtags))
+				{
+					// 直接补全闭合标签
+					$html .= '</' . $openedtags[$i] . '>';
+				}
+				else
+				{
+					unset($closedtags[array_search($openedtags[$i], $closedtags)]);
+				}
+			}
+		}
+		return $html;
 	}
 
 	protected function init_check()
@@ -317,6 +423,8 @@ class Gather
 
 	private function get($url, $data = array(), $showhead = false)
 	{
+		$url = trim(html_entity_decode($url));
+
 		$this->write('开始执行抓取: '.$url);
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -329,6 +437,7 @@ class Gather
 		$error = curl_error($ch);
 		curl_close($ch);
 		unset($ch);
+
 		if (!empty($error))
 		{
 			$this->write('程序抓取URL: '.$url.'发生错误，错误信息: '.$error);
